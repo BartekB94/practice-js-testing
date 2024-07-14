e876fb827b888a5ec538d0e204db3082fa61e3f6
BartekB94 <wurir1994@gmail.com>
2024-07-14 14:16:04 +0200

    02 complete

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', init);
+const alertDisplayEl = document.querySelector('.alert')
 
 function init() {
     const clickEl = document.querySelector('.error--click');
     const enterEl = document.querySelector('.error--enter');
+    alertDisplayEl.classList.add('alert--hidden')
 
     setRandomPosition(clickEl);
     setRandomPosition(enterEl);
@@ -10,19 +12,35 @@ function init() {
     initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
     initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 
+    alertDisplayEl.addEventListener('click', (e) => {
+        if(!e.target.classList.contains('alert__container') && !e.target.classList.contains('alert__message')) {
+            alertDisplayEl.classList.add('alert--hidden')
+        }
+    })
 }
 
 function setRandomPosition(element, error = null) {
     element.style.top = Math.random() * 600 + 'px';
     element.style.left = Math.random() * 800 + 'px';
 
-    if(error) {
+    if (error) {
         throw error;
     }
 }
 
-function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
+function initEventWithError(element, eventName, error) {    
+    element.addEventListener(eventName, function () {
+        try {
+            setRandomPosition(this, error);
+        } catch (e) {
+            displayError(e.message)
+        }
     })
+}
+
+function displayError(error) {
+    const alertMsgEl = document.querySelector('.alert__message')
+
+    alertMsgEl.textContent = error
+    alertDisplayEl.classList.remove('alert--hidden')
 }
