190517884e9faf4796ade1b0b6410bdbb862bc69
BartekB94 <wurir1994@gmail.com>
2024-07-14 17:18:41 +0200

    test parametrs type

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,4 +4,7 @@ describe('randomNumber', () => {
     test('return 1 when min=1 and max=1', () => {
         expect(randomNumber(1, 1)).toBe(1)
     })
+    test('throw error if min/max is not a number', () => {
+        expect(randomNumber('hello', 1)).toThrow()
+    })
 })
