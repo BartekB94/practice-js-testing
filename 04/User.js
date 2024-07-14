e2d622f3a35ac2b0b1438ad13d730674e6da7d60
BartekB94 <wurir1994@gmail.com>
2024-07-14 18:17:43 +0200

    tests passed 04 complete

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,36 @@
 export default class User {
+    constructor({ email, password }) {
+        this.email = email
+        this.password = password
+        this.validateEmail()
+        this.validatePassword()
+    }
 
+    getEmail() {
+        return this.email
+    }
+
+    getPassword() {
+        return this.password
+    }
+
+    validateEmail() {
+        if (!this.email.includes('@')) {
+            throw new Error('Email is not correct')
+        }
+    }
+
+    validatePassword() {
+        if (this.password.length < 8) {
+            throw new Error('Password should have minimum of 8 symbols')
+        }
+    }
+
+    login() {
+        if (this.email.includes('devmentor.pl')) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
